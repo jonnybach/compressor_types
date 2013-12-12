09ce23419fd5a9f6365727b863d13d0db7dae8ec
Jonathan Bachmann <jonathan.bachmann@siemens.com>
2013-12-12 11:28:35 -0500

    v0.7.3 - Added Info.h file to contain library information

---
+++ src/Info.h
@@ -0,0 +1,19 @@
+/*
+ * Info.h
+ *
+ *  Created on: Dec 12, 2013
+ *      Author: bachm03j
+ */
+
+#ifndef INFO_H_
+#define INFO_H_
+
+namespace libSieCompInfo
+{
+    const char* version = {"0.7.3"};
+    const char* libName = {"SieComp"};
+    const char* descrip = {"Library for storing compressor tool related data in an object oriented perspective."};
+}
+
+
+#endif /* INFO_H_ */
